f696ab25a96763e4404fe270985fb4400ae6484c
ZusmanOne <bigmama93@mail.ru>
2020-12-20 22:05:59 +0300

    add postgresql

--- Myblog/Events/models.py
+++ Myblog/Events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Event(models.Model):
+    event_images = models.ImageField(upload_to = 'event-images/')
+    event_text = models.CharField(max_length = 300)
+
 # Create your models here.
--- Myblog/Myblog/settings.py
+++ Myblog/Myblog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'Myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myavesomeblogdb',
+        'USER':'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
     }
 }
 
@@ -118,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/avesome_file/'
